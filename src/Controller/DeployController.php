24b717f43879024aef0a9e6b647b1cf4932878f3
Matt Smith <matt@mattonline.me>
2025-04-01 15:40:38 +0100

    update

--- src/Controller/DeployController.php
+++ src/Controller/DeployController.php
@@ -17,7 +17,7 @@ class DeployController extends AbstractController
     /**
      * @throws TransportExceptionInterface
      */
-    #[Route('/{owner}/{repo}/pull_artifact', ':pull_artifact', methods: ['PUT'])]
+    #[Route('/{owner}/{repo}/pull_artifact', ':pull_artifact')]
     public function pullArtifact(
         string $owner,
         string $repo,
